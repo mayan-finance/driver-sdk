ff39ab56548fe0153e3033680611c77683de4ca4
mrlotfi <mrlotfi@MacBook-Pro.local>
2025-01-31 22:17:40 +0330

    auction changes

--- src/auction.ts
+++ src/auction.ts
@@ -2,22 +2,34 @@ import { getAssociatedTokenAddressSync, TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID 
 import { Connection, PublicKey } from '@solana/web3.js';
 import axios from 'axios';
 import { ethers } from 'ethers6';
-import { CHAIN_ID_SOLANA, isEvmChainId } from './config/chains';
+import { CHAIN_ID_BSC, CHAIN_ID_SOLANA, isEvmChainId, WORMHOLE_DECIMALS } from './config/chains';
 import { RpcConfig } from './config/rpc';
-import { Token } from './config/tokens';
+import { Token, TokenList } from './config/tokens';
 import { WalletConfig } from './config/wallet';
 import { driverConfig } from './driver.conf';
 import { SwapRouters } from './driver/routers';
+import { alertForLossReach, appendLoss, checkPaidLossWithinRange, maxLossPerSwapUSD, removeLoss } from './loss-tracker';
 import { Swap } from './swap.dto';
 import { getErc20Balance } from './utils/erc20';
 import { EvmProviders } from './utils/evm-providers';
 import { SwiftCosts } from './utils/fees';
 import logger from './utils/logger';
+import { AUCTION_MODES } from './utils/state-parser';
 
 export class AuctionFulfillerConfig {
 	private readonly bidAggressionPercent = driverConfig.bidAggressionPercent;
 	private readonly fulfillAggressionPercent = driverConfig.fulfillAggressionPercent;
-	private readonly forceBid = true;
+
+	private readonly perRetryMinAvailableLossUSD: { [x: number]: number } = {
+		0: 0.05,
+		1: 0.1,
+		2: 0.2,
+		3: 0.5,
+		4: 1,
+		5: 5,
+		6: 10,
+	};
+	private readonly forceBid = false;
 
 	constructor(
 		private readonly rpcConfig: RpcConfig,
@@ -25,7 +37,8 @@ export class AuctionFulfillerConfig {
 		private readonly evmProviders: EvmProviders,
 		private readonly walletConfig: WalletConfig,
 		private readonly swapRouters: SwapRouters,
-	) {}
+		private readonly tokenList: TokenList,
+	) { }
 
 	async normalizedBidAmount(
 		driverToken: Token,
@@ -35,7 +48,7 @@ export class AuctionFulfillerConfig {
 	): Promise<bigint> {
 		const balance = await this.getTokenBalance(driverToken);
 		if (balance < effectiveAmountIn) {
-			throw new Error(`Insufficient balance for ${swap.sourceTxHash}. Dropping bid`);
+			throw new Error(`Insufficient 1x balance for ${swap.sourceTxHash}. Dropping bid`);
 		} else {
 			logger.info(`Balance is ${balance} for ${swap.sourceTxHash}`);
 		}
@@ -44,7 +57,15 @@ export class AuctionFulfillerConfig {
 			throw new Error(`Volume limit exceeded for ${swap.sourceTxHash} and dropping bid`);
 		}
 
+		if (
+			swap.sourceChain === CHAIN_ID_BSC &&
+			swap.fromTokenAddress === '0x55d398326f99059ff775485246999027b3197955'
+		) {
+			effectiveAmountIn = this.normalizeUsdtIfRequired(effectiveAmountIn);
+		}
+
 		const normalizedMinAmountOut = BigInt(swap.minAmountOut64);
+		let minAmountOutNum = Number(normalizedMinAmountOut) / 10 ** Math.min(WORMHOLE_DECIMALS, swap.toToken.decimals);
 
 		let output64: bigint;
 		if (swap.destChain === CHAIN_ID_SOLANA) {
@@ -60,6 +81,24 @@ export class AuctionFulfillerConfig {
 			);
 		}
 		let output = Number(output64) / 10 ** swap.toToken.decimals;
+
+		if (costs.fromTokenPrice && costs.toTokenPrice) {
+			const priceRatio = costs.fromTokenPrice / costs.toTokenPrice;
+			if (output / effectiveAmountIn > priceRatio * 1) {
+				const guardedOutput = priceRatio * effectiveAmountIn * 1;
+				output = guardedOutput;
+				logger.warn(
+					`Oracle price differs 2.5% from aggregator. limiting bid to price for ${swap.sourceTxHash}`,
+				);
+			}
+		}
+		if (output > minAmountOutNum * 1.1) {
+			const guardedOutput = minAmountOutNum * 1.1;
+			output = guardedOutput;
+			logger.warn(
+				`Output is too much more than minAmountOut. limiting bid to minAmountOut * 1.1 for ${swap.sourceTxHash}`,
+			);
+		}
 
 		const bpsFees = await this.calcProtocolAndRefBps(
 			swap.fromAmount64,
@@ -78,20 +117,32 @@ export class AuctionFulfillerConfig {
 		const mappedMinAmountIn = minAmountNeededForFulfill * (effectiveAmountIn / output);
 		const mappedBpsAmountIn = (swap.fromAmount.toNumber() * Number(bpsFees)) / 10000; // upper estimate
 
-		if (mappedMinAmountIn > effectiveAmountIn - mappedBpsAmountIn) {
+		if (mappedMinAmountIn > effectiveAmountIn * 1.0003) {
 			logger.warn(
-				`AuctionFulfillerConfig.normalizedBidAmount: mappedMinAmountIn > effectiveAmountIn ${mappedMinAmountIn} > ${effectiveAmountIn}`,
+				`Auction.normalizedBidAmount: mappedMinAmountIn > effectiveAmountIn ${mappedMinAmountIn} > ${effectiveAmountIn} for ${swap.sourceTxHash}`,
 			);
 			throw new Error(`mappedMinAmountIn > effectiveAmountIn for ${swap.sourceTxHash}`);
+			// continute anyway to bid min  amount out
 		}
 
 		const bidAggressionPercent = this.bidAggressionPercent; // 0 - 100
 
 		const profitMargin = effectiveAmountIn - mappedBpsAmountIn;
 
-		const finalAmountIn = mappedMinAmountIn + (profitMargin * bidAggressionPercent) / 100 - mappedBpsAmountIn;
+		const marginFinalBidIn = mappedMinAmountIn + (profitMargin * bidAggressionPercent) / 100;
+		const marginAmountOut = (marginFinalBidIn * Number(output)) / effectiveAmountIn;
 
-		const mappedAmountOut = (finalAmountIn * Number(output)) / effectiveAmountIn;
+		let bidBpsMargin = 3; // 11 bps
+		if (swap.toToken.contract === driverToken.contract) {
+			bidBpsMargin = 3; // 5 bps if no swap is included
+		} else if (!swap.toToken.pythUsdPriceId) {
+			bidBpsMargin = 75; // 75 bps if no pyth price id (probably meme coin,...)
+		}
+		const finalFullAmountIn = (1 - bidBpsMargin / 10000) * (effectiveAmountIn - mappedBpsAmountIn);
+		const fullMappedAmountOut = (finalFullAmountIn * Number(output)) / effectiveAmountIn; // 20 bps test for now
+
+		const mappedAmountOut = Math.max(marginAmountOut, fullMappedAmountOut);
+		swap.bidAmountIn = Math.max(marginFinalBidIn, finalFullAmountIn + mappedBpsAmountIn);
 		let normalizedAmountOut;
 		if (swap.toToken.decimals > 8) {
 			normalizedAmountOut = BigInt(Math.floor(mappedAmountOut * 10 ** 8));
@@ -102,6 +153,7 @@ export class AuctionFulfillerConfig {
 		if (normalizedAmountOut < normalizedMinAmountOut && this.forceBid) {
 			logger.warn(`normalizedBidAmount is less than minAmountOut`);
 			normalizedAmountOut = normalizedMinAmountOut;
+			swap.bidAmountIn = mappedMinAmountIn;
 		}
 
 		return normalizedAmountOut;
@@ -167,9 +219,28 @@ export class AuctionFulfillerConfig {
 		return output;
 	}
 
+	private normalizeUsdtIfRequired(effectiveAmountIn: number) {
+		let usdtToUsdc = this.tokenList.getLatestUsdtToUsdcPrice();
+		if (usdtToUsdc) {
+			let safeUsdtToUsdc = Math.min(1.003, usdtToUsdc);
+			// 10 bps + ratio
+			return effectiveAmountIn * 0.999 * safeUsdtToUsdc;
+		}
+		return effectiveAmountIn / 1.002;
+	}
+
 	async fulfillAmount(driverToken: Token, effectiveAmountIn: number, swap: Swap, costs: SwiftCosts): Promise<number> {
+		checkPaidLossWithinRange(swap.sourceTxHash);
+
 		if (swap.fromAmount.toNumber() * costs.fromTokenPrice > driverConfig.volumeLimitUsd) {
 			throw new Error(`Volume limit exceeded for ${swap.sourceTxHash} and dropping fulfill`);
+		}
+
+		if (
+			swap.sourceChain === CHAIN_ID_BSC &&
+			swap.fromTokenAddress === '0x55d398326f99059ff775485246999027b3197955'
+		) {
+			effectiveAmountIn = this.normalizeUsdtIfRequired(effectiveAmountIn);
 		}
 
 		const normalizedMinAmountOut = BigInt(swap.minAmountOut64);
@@ -200,25 +271,83 @@ export class AuctionFulfillerConfig {
 			swap.toToken.decimals > 8
 				? normalizedMinAmountOut * BigInt(10 ** (swap.toToken.decimals - 8))
 				: normalizedMinAmountOut;
-		const minAmountNeededForFulfill64 = realMinAmountOut + (realMinAmountOut * bpsFees) / 10000n;
-		const minAmountNeededForFulfill = Number(minAmountNeededForFulfill64) / 10 ** swap.toToken.decimals;
+		const minAmountNeededForFulfill64 = Number(realMinAmountOut) / (1 - Number(bpsFees) / 10000);
+		const minAmountNeededForFulfill = (1.000001 * minAmountNeededForFulfill64) / 10 ** swap.toToken.decimals;
 
 		const mappedMinAmountIn = minAmountNeededForFulfill * (effectiveAmountIn / output);
 
-		if (mappedMinAmountIn > effectiveAmountIn) {
-			logger.warn(
-				`AuctionFulfillerConfig.normalizedBidAmount: mappedMinAmountIn > effectiveAmountIn ${mappedMinAmountIn} > ${effectiveAmountIn}`,
-			);
-			throw new Error(`mappedMinAmountIn > effectiveAmountIn for ${swap.sourceTxHash}`);
+		if (!swap.bidAmountIn) {
+			if (swap.bidAmount64) {
+				const bidOut = Number(swap.bidAmount64) / 10 ** Math.min(swap.toToken.decimals, WORMHOLE_DECIMALS);
+				swap.bidAmountIn = (1.000001 * bidOut * (effectiveAmountIn / output)) / (1 - Number(bpsFees) / 10000);
+			}
+		}
+
+		const minFulfillAmount = Math.max(mappedMinAmountIn, swap.bidAmountIn || 0);
+		// for simple mode without auction, do not pay any loss
+		if (swap.auctionMode === AUCTION_MODES.ENGLISH && (minFulfillAmount > effectiveAmountIn || swap.retries > 1)) {
+			if (swap.lastloss && swap.lastloss > 0) {
+				removeLoss(swap.lastloss);
+			}
+
+			let perRetryAddedLossUsd = 0;
+			if (driverToken.contract !== swap.toToken.contract) {
+				perRetryAddedLossUsd = this.perRetryMinAvailableLossUSD[swap.retries];
+			}
+
+			let lossAmountUsd =
+				perRetryAddedLossUsd + costs.fromTokenPrice * Math.max(minFulfillAmount - effectiveAmountIn, 0);
+
+			if (lossAmountUsd > maxLossPerSwapUSD) {
+				logger.warn(`Max loss filled can not for ${minFulfillAmount} > ${effectiveAmountIn}`);
+				alertForLossReach('perSwapLoss', `Max loss filled for one swap pls check ${swap.sourceTxHash}`);
+				throw new Error(`max per-swap loss filled (need ${lossAmountUsd})  for  ${swap.sourceTxHash}`);
+			}
+
+			if (lossAmountUsd > costs.fromTokenPrice * effectiveAmountIn * 0.1) {
+				logger.info(
+					`MAX10 capped for ${swap.sourceTxHash},  ${lossAmountUsd} loss reduced to ${costs.fromTokenPrice * effectiveAmountIn * 0.1}`,
+				);
+				throw Error(`Would not fulfill because capped loss ${swap.sourceTxHash}`);
+			}
+
+			logger.info(`Loss of ${lossAmountUsd} USD is going to be appended for ${swap.sourceTxHash}`);
+			appendLoss(lossAmountUsd);
+			swap.lastloss = lossAmountUsd;
+
+			// we need a safeguard to prevent divison by small values in case we get wrong prices from server, ...
+			const minPrices = {
+				usdc: 0.9,
+				eth: 2500,
+			};
+			let safeFromTokenPriceForAddedLoss = costs.fromTokenPrice;
+			if (
+				driverToken.contract === ethers.ZeroAddress ||
+				driverToken.contract === '7vfCXTUXx5WJV5JADk17DUJ4ksgau7utNKj4b963voxs' // weth sol
+			) {
+				// eth
+				safeFromTokenPriceForAddedLoss = Math.max(minPrices.eth, costs.fromTokenPrice);
+			} else {
+				safeFromTokenPriceForAddedLoss = Math.max(minPrices.usdc, costs.fromTokenPrice);
+			}
+
+			effectiveAmountIn =
+				Math.max(effectiveAmountIn, minFulfillAmount * 1.0001) +
+				perRetryAddedLossUsd / safeFromTokenPriceForAddedLoss;
+		} else if (minFulfillAmount > effectiveAmountIn) {
+			throw new Error(`Insufficient effective amount in for ${swap.sourceTxHash}. dropping fulfill`);
 		}
 
 		const aggressionPercent = this.fulfillAggressionPercent; // 0 - 100
 
 		const profitMargin = effectiveAmountIn - mappedMinAmountIn;
 
-		const finalAmountIn = mappedMinAmountIn + (profitMargin * aggressionPercent) / 100;
+		let finalAmountIn = mappedMinAmountIn + (profitMargin * aggressionPercent) / 100;
+		if (finalAmountIn * (1 - 3.1 / 10000) > minFulfillAmount) {
+			finalAmountIn = finalAmountIn * (1 - 3 / 10000);
+		}
 
-		return finalAmountIn;
+		return Math.max(finalAmountIn, minFulfillAmount * 1.000001);
 	}
 
 	private async getJupQuoteWithRetry(
