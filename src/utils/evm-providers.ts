b442e92437e889619f5d6fca14210a759b7fae62
Mohammad Reza Lotfi <mohamadmlnnamin@gmail.com>
2025-02-17 17:09:18 +0330

    Add rpc failover (#35)
    Co-authored-by: mrlotfi <mrlotfi@MacBook-Pro.local>

--- src/utils/evm-providers.ts
+++ src/utils/evm-providers.ts
@@ -12,78 +12,130 @@ import { RpcConfig } from '../config/rpc';
 
 export type EvmProviders = { [evmNetworkId: number | string]: ethers.JsonRpcProvider };
 
-export function makeEvmProviders(chainIds: number[], rpcConfig: RpcConfig): EvmProviders {
-	const result: { [key: number]: ethers.JsonRpcProvider } = {};
+let activeProviders: { [chain: number]: ethers.JsonRpcProvider } = {};
+let allProviders: { [chain: number]: ethers.JsonRpcProvider[] } = {};
 
-	for (const chainId of chainIds) {
-		if (chainId === CHAIN_ID_BSC) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.bsc, 56, {
-				staticNetwork: ethers.Network.from(56),
-			});
-		} else if (chainId === CHAIN_ID_POLYGON) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.polygon, 137, {
-				staticNetwork: ethers.Network.from(137),
-			});
-		} else if (chainId === CHAIN_ID_ETH) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.ethereum, 1, {
-				staticNetwork: ethers.Network.from(1),
-			});
-		} else if (chainId === CHAIN_ID_AVAX) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.avalanche, 43114, {
-				staticNetwork: ethers.Network.from(43114),
-			});
-		} else if (chainId === CHAIN_ID_ARBITRUM) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.arbitrum, 42161, {
-				staticNetwork: ethers.Network.from(42161),
-			});
-		} else if (chainId === CHAIN_ID_OPTIMISM) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.optimism, 10, {
-				staticNetwork: ethers.Network.from(10),
-			});
-		} else if (chainId === CHAIN_ID_BASE) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.base, 8453, {
-				staticNetwork: ethers.Network.from(8453),
-			});
+async function setFirstActiveProvider() {
+	for (let chainId of Object.keys(allProviders)) {
+		for (let provider of allProviders[+chainId]) {
+			try {
+				await provider.getBlockNumber();
+				activeProviders[+chainId] = provider;
+				break;
+			} catch (e) {
+				console.error(`Error when trying to find active chain rpc ${e}`);
+			}
 		}
 	}
-
-	return result;
 }
 
-export function makeSecondEvmProviders(chainIds: number[], rpcConfig: RpcConfig): EvmProviders {
-	const result: { [key: number]: ethers.JsonRpcProvider } = {};
+export async function refreshEvmProvidersPeriodically() {
+	while (true) {
+		try {
+			await setFirstActiveProvider();
+			await setFirstActive2nProvider();
+			await new Promise((resolve) => setTimeout(resolve, 15_000));
+		} catch (err) {
+			console.error(`Error when trying to refresh evm providers ${err}`);
+		}
+	}
+}
 
+export async function makeEvmProviders(chainIds: number[], rpcConfig: RpcConfig): Promise<EvmProviders> {
 	for (const chainId of chainIds) {
+		allProviders[chainId] = [];
+		let realChainId: number = -1;
+		let endpoints: string[] = [];
 		if (chainId === CHAIN_ID_BSC) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.bsc2nd, 56, {
-				staticNetwork: ethers.Network.from(56),
-			});
+			realChainId = 56;
+			endpoints = rpcConfig.evmEndpoints.bsc.split(',');
 		} else if (chainId === CHAIN_ID_POLYGON) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.polygon2nd, 137, {
-				staticNetwork: ethers.Network.from(137),
-			});
+			realChainId = 137;
+			endpoints = rpcConfig.evmEndpoints.polygon.split(',');
 		} else if (chainId === CHAIN_ID_ETH) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.ethereum2nd, 1, {
-				staticNetwork: ethers.Network.from(1),
-			});
+			realChainId = 1;
+			endpoints = rpcConfig.evmEndpoints.ethereum.split(',');
 		} else if (chainId === CHAIN_ID_AVAX) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.avalanche2nd, 43114, {
-				staticNetwork: ethers.Network.from(43114),
-			});
+			realChainId = 43114;
+			endpoints = rpcConfig.evmEndpoints.avalanche.split(',');
 		} else if (chainId === CHAIN_ID_ARBITRUM) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.arbitrum2nd, 42161, {
-				staticNetwork: ethers.Network.from(42161),
-			});
+			realChainId = 42161;
+			endpoints = rpcConfig.evmEndpoints.arbitrum.split(',');
 		} else if (chainId === CHAIN_ID_OPTIMISM) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.optimism2nd, 10, {
-				staticNetwork: ethers.Network.from(10),
-			});
+			realChainId = 10;
+			endpoints = rpcConfig.evmEndpoints.optimism.split(',');
 		} else if (chainId === CHAIN_ID_BASE) {
-			result[chainId] = new ethers.JsonRpcProvider(rpcConfig.evmEndpoints.base2nd, 8453, {
-				staticNetwork: ethers.Network.from(8453),
+			realChainId = 8453;
+			endpoints = rpcConfig.evmEndpoints.base.split(',');
+		}
+
+		for (let endpoint of endpoints) {
+			const provider = new ethers.JsonRpcProvider(endpoint, realChainId, {
+				staticNetwork: ethers.Network.from(realChainId),
 			});
+			allProviders[chainId].push(provider);
 		}
 	}
 
-	return result;
+	await setFirstActiveProvider();
+
+	return activeProviders;
+}
+
+let active2ndProviders: { [chain: number]: ethers.JsonRpcProvider } = {};
+let all2ndProviders: { [chain: number]: ethers.JsonRpcProvider[] } = {};
+
+async function setFirstActive2nProvider() {
+	for (let chainId of Object.keys(all2ndProviders)) {
+		for (let provider of all2ndProviders[+chainId]) {
+			try {
+				await provider.getBlockNumber();
+				active2ndProviders[+chainId] = provider;
+				break;
+			} catch (e) {
+				console.error(`Error when trying to find active  2ndry chain rpc ${e}`);
+			}
+		}
+	}
+}
+
+export async function makeSecondEvmProviders(chainIds: number[], rpcConfig: RpcConfig): Promise<EvmProviders> {
+	let realChainId: number = -1;
+	let endpoints: string[] = [];
+
+	for (const chainId of chainIds) {
+		if (chainId === CHAIN_ID_BSC) {
+			realChainId = 56;
+			endpoints = rpcConfig.evmEndpoints.bsc2nd.split(',');
+		} else if (chainId === CHAIN_ID_POLYGON) {
+			realChainId = 137;
+			endpoints = rpcConfig.evmEndpoints.polygon2nd.split(',');
+		} else if (chainId === CHAIN_ID_ETH) {
+			realChainId = 1;
+			endpoints = rpcConfig.evmEndpoints.ethereum2nd.split(',');
+		} else if (chainId === CHAIN_ID_AVAX) {
+			realChainId = 43114;
+			endpoints = rpcConfig.evmEndpoints.avalanche2nd.split(',');
+		} else if (chainId === CHAIN_ID_ARBITRUM) {
+			realChainId = 42161;
+			endpoints = rpcConfig.evmEndpoints.arbitrum2nd.split(',');
+		} else if (chainId === CHAIN_ID_OPTIMISM) {
+			realChainId = 10;
+			endpoints = rpcConfig.evmEndpoints.optimism2nd.split(',');
+		} else if (chainId === CHAIN_ID_BASE) {
+			realChainId = 8453;
+			endpoints = rpcConfig.evmEndpoints.base2nd.split(',');
+		}
+
+		for (let endpoint of endpoints) {
+			const provider = new ethers.JsonRpcProvider(endpoint, realChainId, {
+				staticNetwork: ethers.Network.from(realChainId),
+			});
+			all2ndProviders[chainId] = [provider];
+		}
+
+		await setFirstActive2nProvider();
+	}
+
+	return active2ndProviders;
 }
