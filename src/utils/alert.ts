c8e7663906ac73cd07307b6cd3c0ae2f355882cc
mrlotfi <mrlotfi@MacBook-Pro.local>
2025-04-06 17:12:27 +0330

    Add missing alert util

---
+++ src/utils/alert.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export async function sendAlert(code: string, msg: string) {
+	try {
+		await axios.get(`${process.env.ALERT_FULL_URL}?pwd=${process.env.ALERT_PWD}&code=${code}&message=${msg}`);
+	} catch (err) {
+		console.warn('Failed to send alert', err);
+	}
+}
